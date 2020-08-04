963fa9442db5571d65f806b8c2e59319b2e18b6e
ehza <ehzawad@gmail.com>
2020-08-04 11:48:02 +0600

    Update oldphoto.m

--- oldphoto.m
+++ oldphoto.m
@@ -1,14 +1,19 @@
 
+
 function T = oldphoto(ImageEm)
-    oldRedChannel = ImageEm(:, :, 1);
-    oldGreenChannel = ImageEm(:, :, 2);
-    oldBlueChannel = ImageEm(:, :, 3);
     
-    newRed = (oldRedChannel * .393) + (oldGreenChannel * .769) + (oldBlueChannel * .189);
-    newGreen = (oldRedChannel * .349) + (oldGreenChannel * .686) + (oldBlueChannel * .168);
-    newBlue = (oldRedChannel * .272) + (oldGreenChannel * .534) + (oldBlueChannel * .131);
+    [r, g, b] = separateChannel(ImageEm);
+    
+    newRed = (r * .393) + (g * .769) + (b * .189);
+    newGreen = (r * .349) + (g * .686) + (b * .168);
+    newBlue = (r * .272) + (g * .534) + (b * .131);
+    
     
     T = cat(3, newRed, newGreen, newBlue);
 end
 
-
+function [r, g, b] = separateChannel(I)
+    r = I(:, :, 1);
+    g = I(:, :, 2);
+    b = I(:, :, 3);
+end
